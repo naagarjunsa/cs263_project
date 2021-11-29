3cd618b538d93bdf8a3a05cd729371a290dd2807
Nagarjun <nagarjun@ucsb.edu>
2021-11-28 19:58:16 -0800

    go cpu io added

--- go/go-web-server/main.go
+++ go/go-web-server/main.go
@@ -9,12 +9,19 @@ import (
 )
 
 func main() {
-	var count = 0
-	// set router
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		defer r.Body.Close()
-		count++
-		handleConnection(w, count)
+		handleConnection(w, "/")
+	})
+
+	http.HandleFunc("/io", func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+		handleConnection(w, "/io")
+	})
+
+	http.HandleFunc("/cpu", func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+		handleConnection(w, "/cpu")
 	})
 	// set listen port
 	err := http.ListenAndServe(":8080", nil)
@@ -23,14 +30,21 @@ func main() {
 	}
 }
 
-func handleConnection(w http.ResponseWriter, count int) {
+func handleConnection(w http.ResponseWriter, url string) {
 	// add 2 second delay to every 10th request
-	if (count % 10) == 0 {
-		println("Adding delay. Count: ", count)
+	if url == "/cpu" {
 		time.Sleep(2 * time.Second)
+		w.Header().Add("Connection", "keep-alive")
+		w.WriteHeader(200)
+		fmt.Fprintf(w, "CPU Intensive Task Done!")
+	} else if url == "/" {
+		w.Header().Add("Connection", "keep-alive")
+		w.WriteHeader(200)
+		fmt.Fprintf(w, "Hello World!")
+	} else if url == "/io" {
+		html, _ := ioutil.ReadFile("hello.html") 
+		w.Header().Add("Connection", "keep-alive")
+		w.WriteHeader(200)           
+		fmt.Fprintf(w, string(html))
 	}
-	html, _ := ioutil.ReadFile("hello.html") // read html file
-	w.Header().Add("Connection", "keep-alive")
-	w.WriteHeader(200)           // 200 OK
-	fmt.Fprintf(w, string(html)) // send data to client side
 }
