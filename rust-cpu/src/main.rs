c1aef9dfe6ee0c63cfbb207ebee85af18aa87d43
Nagarjun <nagarjun@ucsb.edu>
2021-11-29 03:29:03 -0800

    rust file io fix-2

--- rust-cpu/src/main.rs
+++ rust-cpu/src/main.rs
@@ -1,12 +1,12 @@
 use warp::Filter;
 use std::time::Duration;
-use std::thread::sleep;
+use async_std::task;
 
 #[tokio::main]
 async fn main() {
     let routes = warp::any().map(|| {     
-                        sleep(Duration::from_millis(250)).await;
-                        return "CPU Intensive task done!"});
+        task::sleep(Duration::from_secs(0.25)).await;
+        return "CPU Intensive task done!"});
 
     warp::serve(routes)
         .run(([127, 0, 0, 1], 8080))
